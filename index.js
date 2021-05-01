89f7486fe3bf4d8760b48f36da5418d7f4bd2a50
Ilya <ilia200816@gmail.com>
2021-05-01 12:10:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js'); // connectiong Discord.Js [NPM I DISCORD.
 const client = new Discord.Client();
 const prefix = require("./config.json").auth.prefix; // Linking to the auth directory of the config.json
 const token = require("./config.json").auth.token;
-const ytdl = require('ytdl-core') // connecting bot to the ytdl core to create music commands [optional]
 require("./functions.js")(client);
 client.commands = new Discord.Collection;
 client.aliases = new Discord.Collection;
