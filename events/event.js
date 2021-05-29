7283a3e1ae4e27e7c49c2a3deb40a3bc00338378
X_Solis_X <45941177+xsolisortusx@users.noreply.github.com>
2021-05-29 14:21:21 +0300

    Update event.js

--- events/event.js
+++ events/event.js
@@ -1,11 +1,10 @@
-/* Example of the file in ./events/
-const Discord = require('discord.js') Require Discord.js
-var {client} = require('../index.js') Getting client
-const chalk = require('chalk') OPTIONALLY: Download chalk (npm i chalk) for better console.log
-client.on("ready", async () => { -- creating event
-    console.log(chalk.blueBright('{Event:start} Ready')) -- console log of event
+/* Example of the file in ./events/ */
+const Discord = require('discord.js') // Require Discord.js
+var {client} = require('../index.js')//Getting client
+const chalk = require('chalk') //OPTIONALLY: Download chalk (npm i chalk) for better console.log
+client.on("ready", async () => { // creating event
+    console.log(chalk.blueBright('{Event:start} Ready')) // console log of event
     setTimeout(() => {
-        client.user.setPresence({ status: "idle", activity: { name: "Status of bot", type: "WATCHING"}})
+        client.user.setPresence({ status: "idle", activity: { name: "Made by UAV#2529", type: "WATCHING"}})
     }, 1000)
 }); 
-*/
